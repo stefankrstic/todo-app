b242946a65fbaa181978bc806fc2852ce0e3d76e
stefankrstic <stefan.k.krstic@gmail.com>
2024-05-07 18:57:23 +0200

    Actions and reducers

---
+++ src/redux/actions.js
@@ -0,0 +1,49 @@
+import {
+  ADD_TODO,
+  FILTER_TODOS,
+  MARK_ALL_COMPLETED,
+  MARK_COMPLETED,
+  MARK_INCOMPLETE,
+  REMOVE_TODO,
+  TOGGLE_TODO,
+  UPDATA_SERACH_TERM,
+} from "./actionTypes";
+
+export const addTodo = (text) => ({
+  type: ADD_TODO,
+  payload: { text },
+});
+
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
+  payload: { id },
+});
+
+export const removeTodo = (id) => ({
+  type: REMOVE_TODO,
+  payload: { id },
+});
+
+export const markCompleted = (id) => ({
+  type: MARK_COMPLETED,
+  payload: { id },
+});
+
+export const markIncomplete = (id) => ({
+  type: MARK_INCOMPLETE,
+  payload: { id },
+});
+
+export const filterTodos = (filter) => ({
+  type: FILTER_TODOS,
+  payload: { filter },
+});
+
+export const markAllCompleted = () => ({
+  type: MARK_ALL_COMPLETED,
+});
+
+export const updateSearchTerm = (searchTerm) => ({
+  type: UPDATA_SERACH_TERM,
+  payload: { searchTerm },
+});
